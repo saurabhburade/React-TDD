6e052443cc8aa55badbf5ee3b425a10fde41bde3
saurabhburade <sburade3@gmail.com>
2022-01-09 16:12:08 +0530

    Add counter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const handleIncrement = () => {
+    setCounter(counter + 1);
+  };
   return (
     <div className="App">
-      <h1>Learn react TDD</h1>
+      <h1>Count : {counter}</h1>
+      <button onClick={handleIncrement}>Increment</button>
     </div>
   );
 }
